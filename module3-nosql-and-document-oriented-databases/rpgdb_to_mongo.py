26d74701e68b81ebf82bafff6d93a58481fe1dab
modares <modareshadi@gmail.com>
2020-05-14 14:13:54 -0700

    new_db

--- module3-nosql-and-document-oriented-databases/rpgdb_to_mongo.py
+++ module3-nosql-and-document-oriented-databases/rpgdb_to_mongo.py
@@ -13,32 +13,21 @@ DB_USER = os.getenv("MONGO_USER", default="OOPS")
 DB_PASSWORD = os.getenv("MONGO_PASSWORD", default="OOPS")
 CLUSTER_NAME = os.getenv("MONGO_CLUSTER_NAME", default="OOPS")
 
-con = sqlite3.connect("./rpg_db.sqlite3")
-query_string = "SELECT * FROM armory_item"
-df = pd.read_sql_query(query_string, con)
+sqlite_con = sqlite3.connect("./rpg_db.sqlite3")
+query_string = "SELECT * FROM charactercreator_mage"
+df = pd.read_sql_query(query_string, sqlite_con)
 
 
-connection_uri = f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{CLUSTER_NAME}.mongodb.net/test?retryWrites=true&w=majority"
+pymongo_connection_uri = f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{CLUSTER_NAME}.mongodb.net/test?retryWrites=true&w=majority"
 
-print("URI:", connection_uri)
+print("URI:", pymongo_connection_uri)
 
-client = pymongo.MongoClient(connection_uri)
-print("----------------")
-print("CLIENT:", type(client), client)
+client = pymongo.MongoClient(pymongo_connection_uri)
 
-db = client.test_database  # "test_database" or whatever you want to call it
-print("----------------")
-print("DB:", type(db), db)
+db = client["rpg_db"]
 
-collection = db.armory_item  # "pokemon_test" or whatever you want to call it
-print("----------------")
-print("COLLECTION:", type(collection), collection)
+collection = db["charactercreator_mage"]
 
-print("----------------")
-print("COLLECTIONS:")
 print(db.list_collection_names())
 
-# df = pd.read_csv("./titanic.csv")
 collection.insert_many(df.to_dict(orient='records'))
-# print("DOCS:", collection.count_documents({}))
-# print(collection.count_documents({"name": "Pikachu"}))
